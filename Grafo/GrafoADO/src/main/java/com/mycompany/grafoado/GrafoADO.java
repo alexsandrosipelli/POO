14fe0cc8b15e297cf45e5835baceabf62697462f
HiagoPereiraAnjos <hiagopereira22@gmail.com>
2023-11-10 06:58:59 -0300

    Adicionando a View

--- Grafo/GrafoADO/src/main/java/com/mycompany/grafoado/GrafoADO.java
+++ Grafo/GrafoADO/src/main/java/com/mycompany/grafoado/GrafoADO.java
@@ -19,36 +19,67 @@ public class GrafoADO {
 
     public static void main(String[] args) {
         Grafo<String> grafo = new Grafo<String>();
-        String nomeVertice;
-        String esquinaInicio;
-        String esquinaFim;
-        double tempoPercurso;        
-        int escolha;
-        while (true) {
 
-            escolha = Integer.parseInt(JOptionPane.showInputDialog(null, "Escolha a opção\n"
-                    + " 1- Inserir ponto de partida \n"
-                    + " 2- Iserir conexao e tempo de percurso \n"
-                    + " 3- Ver mapa atual \n"
-                    + " 4- Sair "));
-            if (escolha == 1) {
-                nomeVertice = JOptionPane.showInputDialog(null, "De nome a esquina");
-                                               
-                grafo.adicionarVertice(nomeVertice);
-            } else if (escolha == 2) {
-                esquinaInicio = JOptionPane.showInputDialog(null, "De qual esquina voce esta partindo?");
-                esquinaFim = JOptionPane.showInputDialog(null, "Qual a esquina que voce chegou?");
-                tempoPercurso = Double.parseDouble(JOptionPane.showInputDialog(null, "Qual foi o tempo ?"));
-                grafo.adicionarAresta(tempoPercurso, esquinaInicio, esquinaFim);
-            } else if (escolha == 3) {
-                grafo.BuscaEmLargura();
-            } else if (escolha == 4) {
-                System.exit(0);
-            } else {
-                JOptionPane.showMessageDialog(null, "Escolha invalida!!");
+        grafo.adicionarVertice("esquina A");
+
+        grafo.adicionarVertice("esquina B");
+
+        grafo.adicionarVertice("esquina C");
+
+        grafo.adicionarVertice("esquina D");
+
+        grafo.adicionarVertice("esquina E");
+
+        grafo.adicionarVertice("esquina F");
+
+        grafo.adicionarVertice("esquina G");
+
+        grafo.adicionarVertice("esquina H");
+
+        grafo.adicionarVertice("esquina I");
+
+        grafo.adicionarVertice("esquina J");
+
+        grafo.adicionarVertice("esquina K");
+
+        grafo.adicionarAresta(10.0, "esquina A", "esquina C");
+        grafo.adicionarAresta(9.0, "esquina A", "esquina B");
+        grafo.adicionarAresta(15.0, "esquina C", "esquina E");
+        grafo.adicionarAresta(4.0, "esquina C", "esquina D");
+        grafo.adicionarAresta(5.0, "esquina D", "esquina F");
+        grafo.adicionarAresta(10.0, "esquina D", "esquina K");
+        grafo.adicionarAresta(20.0, "esquina E", "esquina G");
+        grafo.adicionarAresta(4.0, "esquina E", "esquina F");
+        grafo.adicionarAresta(5.0, "esquina F", "esquina J");
+        grafo.adicionarAresta(10.0, "esquina G", "esquina H");
+        grafo.adicionarAresta(5.0, "esquina H", "esquina I");
+        grafo.adicionarAresta(13.0, "esquina I", "esquina J");
+        grafo.adicionarAresta(5.0, "esquina J", "esquina K");
+        grafo.adicionarAresta(7.0, "esquina K", "esquina B");
+        grafo.adicionarAresta(7.0, "esquina B", "esquina A");
+
+        grafo.BuscaEmLargura();
+
+        Map<String, List<String>> graf = new HashMap<>();
+        graf.put("esquina A", new ArrayList<>(Arrays.asList("esquina C", "esquina B")));
+        graf.put("esquina B", new ArrayList<>(Arrays.asList("esquina A", "esquina K")));
+        graf.put("esquina C", new ArrayList<>(Arrays.asList("esquina A", "esquina E", "esquina D")));
+        graf.put("esquina D", new ArrayList<>(Arrays.asList("esquina C", "esquina F", "esquina K")));
+        graf.put("esquina E", new ArrayList<>(Arrays.asList("esquina C", "esquina G", "esquina F")));
+        graf.put("esquina F", new ArrayList<>(Arrays.asList("esquina D", "esquina E", "esquina J")));
+        graf.put("esquina G", new ArrayList<>(Arrays.asList("esquina E", "esquina H")));
+        graf.put("esquina H", new ArrayList<>(Arrays.asList("esquina G", "esquina I")));
+        graf.put("esquina I", new ArrayList<>(Arrays.asList("esquina H", "esquina J")));
+        graf.put("esquina J", new ArrayList<>(Arrays.asList("esquina F", "esquina I", "esquina K")));
+        graf.put("esquina K", new ArrayList<>(Arrays.asList("esquina D", "esquina J", "esquina B")));
+
+        for (Map.Entry<String, List<String>> entry : graf.entrySet()) {
+            String vertice = entry.getKey();
+            List<String> conexoes = entry.getValue();
+            System.out.print(vertice + " -> ");
+            for (String conexao : conexoes) {
+                System.out.print(conexao + " ");
             }
         }
-
     }
-
 }
